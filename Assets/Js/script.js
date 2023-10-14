1cd9aebbd8e1a0633bc2773a4605c27dec840c2a
Cristina Garcia <garmercy.mar@gmail.com>
2023-10-14 18:30:28 +1100

    adding const

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,8 +1,9 @@
-id="inputSearch"
-id="btnSearch"
-id="cityHistory" 
-id="containerCityWeather"
-id="temperature"
-id="humidity"
-id="wind"
-id="forecastList"
+const inputSearch = document.querySelector("#inputSearch");
+const btnSearch = document.querySelector("#btnSearch");
+const cityHistory = document.querySelector("#cityHistory");
+const containerCityWeather = document.querySelector("#containerCityWeather");
+const temperature = document.querySelector("#temperature");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const forecastList = document.querySelector("#forecastList");
+
