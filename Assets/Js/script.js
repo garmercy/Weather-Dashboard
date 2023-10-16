fd0f1f5260e21f2d338d78635c7590a9e9fe2654
Cristina Garcia <garmercy.mar@gmail.com>
2023-10-16 23:29:21 +1100

    adding js function forecast

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,12 +1,10 @@
-const inputSearch = document.querySelector("#inputSearch");
 const btnSearch = document.querySelector("#btnSearch");
 const cityHistory = document.querySelector("#cityHistory");
 const weatherIcon = document.querySelector(".weather-icon");
 const apiKey = "d3d153c99b7719684bfe14f41dd76a7b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-//https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=d3d153c99b7719684bfe14f41dd76a7b&units=metric
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-//this function call the api
+//This function call the api to check the city searched
 async function chekingWeather (city){
     const response = await fetch (apiUrl + city +`&appid=${apiKey}`);
     var data = await response.json();
@@ -16,6 +14,10 @@ async function chekingWeather (city){
     document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
     document.querySelector("#wind").innerHTML = data.wind.speed + " km/h";
 
+    var currentDateEl = $('#time');
+    var todayDate = moment().format('(D/MM/YY)');
+    currentDateEl.text(todayDate);
+   
     if(data.weather[0].main == "Clouds"){
         weatherIcon.src = "Assets/Css/Images/clouds.png";
     }else if(data.weather[0].main == "Clear")
@@ -29,18 +31,66 @@ async function chekingWeather (city){
     //missing to check the right name "storm"
     else if(data.weather[0].main == "Storm")
     weatherIcon.src = "Assets/Css/Images/storm.png";
+
+    document.querySelector("#containerForecast").style.display = "block";
 }
 
 btnSearch.addEventListener("click", ()=>{
-    chekingWeather(inputSearch.value);
+    chekingWeather(inputSearch.value)+forecast(inputSearch.value);
 })
 
+// This function creates the forecast cards
+function forecast(fore) {
+ 
+    const apiUrl5 ="https://api.openweathermap.org/data/2.5/forecast?q=" + fore + "&appid=" + apiKey;
 
-// const containerCityWeather = document.querySelector("#containerCityWeather");
-//------------------------------------------------------------------
-// const cityName = document.querySelector("#city");
-// const temperature = document.querySelector("#temperature");
-// const humidity = document.querySelector("#humidity");
-// const wind = document.querySelector("#wind");
-//-------------------------------------------------------------------
-// const forecastList = document.querySelector("#forecastList");
+    $.ajax({ url: apiUrl5, type: "GET" }).then(function (response) {
+      var list = response.list;
+      
+      //Using for to create the 5 weather cards
+      $("#forecastList").html("");
+      for (i=0;i<5;i++) {
+        // Date,icon,temp,humidity and wind values
+        let date = new Date(list[i].dt_txt);
+        let iconId = list[i].weather[0].icon;
+        let temp = ((list[i].main.temp - 273.15)).toFixed(2);
+        let humidity = list[i].main.humidity;
+        let wind = list[i].wind.speed;
+        
+        //Getting the date 
+        let month = date.getMonth();
+        let year = date.getFullYear();
+        let dateForecast = `${month +1}/${year}`;
+        console.log(dateForecast);
+
+        //Creating cards in html
+        let col = $("<div>");
+        col.addClass("col");
+        let forecard = $("<div>");
+        forecard.addClass("card");
+
+        //Adding date to html
+        col.append(forecard);
+        let p = $("<p>").text(dateForecast);
+
+        // Calling img icon from the API and additing to html
+        let imgUrl = "https://openweathermap.org/img/wn/" + iconId + "@2x.png";
+        let weatherImage = $("<img>");
+
+        //Adding temp,wind and humidity values to html 
+        weatherImage.attr("src", imgUrl);
+        var p1 = $("<p>").text("Temp: " + temp + "°C");
+        var p2 = $("<p>").text("Humidity: " + humidity + "%");
+        var p3 = $("<p>").text("Wind: " + wind + "km/h");
+  
+        // p,weatherImage appending to forecard
+        forecard.append(p);
+        forecard.append(weatherImage);
+        forecard.append(p1);
+        forecard.append(p2);
+        forecard.append(p3);
+  
+        $("#forecastList").prepend(col);
+      }
+    });
+  }
